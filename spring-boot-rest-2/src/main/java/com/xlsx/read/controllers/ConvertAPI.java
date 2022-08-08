6582f3fd96391fc55325120e6e751b06e37ad335
ashu <recharge.ashu@gmail.com>
2022-08-08 22:23:56 +0530

    WAR Application

---
+++ spring-boot-rest-2/src/main/java/com/xlsx/read/controllers/ConvertAPI.java
@@ -0,0 +1,51 @@
+package com.xlsx.read.controllers;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.JsonObject;
+
+
+@Controller
+public class ConvertAPI {
+	
+	@Value("${app.pathtoexcel}")
+	private String pathtoexcel;
+	
+	@RequestMapping(value = "parseExcel", method = RequestMethod.POST,consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	String auth(@RequestParam Map<String,String> allParams) {
+		System.out.println("Parameters are " + allParams.entrySet());
+		allParams.forEach((k,v) -> System.out.println("Key = " + k + ", Value = " + v));
+		String FILE_NAME = pathtoexcel;
+		FileInputStream excelFile;
+		try {
+			excelFile = new FileInputStream(FILE_NAME);
+			// System.out.println(excel.getExcelDataAsJsonObject(excelFile));
+			    return excel.getExcelDataAsJsonObject(excelFile).toString();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			 JsonObject jsonObject = new JsonObject();
+			 jsonObject.addProperty("ERROR", e.getMessage());
+			 return jsonObject.toString();
+		}
+		
+	}
+	
+}
---
+++ spring-boot-rest-2/src/main/java/com/xlsx/read/server/main/SpringBootRest2Application.java
@@ -0,0 +1,18 @@
+package com.xlsx.read.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = {"com.xlsx"})
+public class SpringBootRest2Application extends SpringBootServletInitializer {
+
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	         return application.sources(SpringBootRest2Application.class);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRest2Application.class, args);
+	}
+}
